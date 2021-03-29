e97faf98cdc1ca2291149228a347872f67c7bf70
Karolis Petreikis <karolis.petreikis@gmail.com>
2021-03-29 13:21:17 +0300

    Reduced polling intervals

--- blocks.def.h
+++ blocks.def.h
@@ -6,9 +6,9 @@ static const Block blocks[] = {
 	{"  ", "/usr/local/bin/dwmblocks_scripts/memory",					10,		0},
 	{"  ", "df | grep '/dev/nvme0n1p7' | awk '{print $5}'",					60,		0},
 	{"  ", "/usr/local/bin/dwmblocks_scripts/load_average",					10,		0},
-	{"", "/usr/local/bin/dwmblocks_scripts/battery2",					5,		0},
+	{"", "/usr/local/bin/dwmblocks_scripts/battery2",					60,		0},
 	{"  ", "/usr/local/bin/dwmblocks_scripts/xkblayout-state print %e | awk '{ print toupper($0) }'",					1,		0},
-	{"  ", "date '+%a, %b %d'",					5,		0},
+	{"  ", "date '+%a, %b %d'",					60,		0},
 	{"  ", "date '+%H:%M'",					5,		0},
 	{"", "echo ''",					3600,		0},
 };
